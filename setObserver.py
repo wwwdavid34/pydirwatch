4f27dd597fdafc99d9e871d50922a658824d5c71
wwwdavid34 <wwwdavid34@gmail.com>
2019-07-29 14:22:59 -0600

    Testing pycharm commit. #2

--- setObserver.py
+++ setObserver.py
@@ -25,7 +25,7 @@ class DirWatch:
         self.dirwatch_dir = dirwatch_dir
         self.options = self.Options()
         self.tmpwatch = self.Tmpwatch()
-        self.scenarios = []aa
+        self.scenarios = []aaaa
         self.observer = Observer()
         self.config_file = config_file
 
